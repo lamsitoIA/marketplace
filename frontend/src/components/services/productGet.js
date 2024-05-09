0f8ec99ae77de130ed7456ed3590f1354938c50a
NertaiL <hans.latoja@gmail.com>
2024-05-08 21:23:35 -0400

    test in desarrollo

--- frontend/src/components/services/productGet.js
+++ frontend/src/components/services/productGet.js
@@ -1,6 +1,6 @@
 // Reemplazar por la URL de la API
-const URL_API = "http://localhost:3000/api/v1/products"; 
-//const URL_API = "https://marketplace-if9n.onrender.com/api/v1/products";
+//const URL_API = "http://localhost:3000/api/v1/products"; 
+const URL_API = "http://marketplace-if9n.onrender.com/api/v1/products";
               
 export const productsGet = async () => {
   
