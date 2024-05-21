61680ea6544d6d9623c05a9ebcca3293f9bfef46
NertaiL <hans.latoja@gmail.com>
2024-05-20 20:48:44 -0400

    comment

--- frontend/src/components/services/productPut.js
+++ frontend/src/components/services/productPut.js
@@ -1,4 +1,3 @@
-import { GiConsoleController } from "react-icons/gi";
 import axios from "axios";
 // Reemplazar por la URL de la API
 const URL_API = "http://localhost:3000/api/v1/products";
@@ -7,6 +6,7 @@ const URL_API = "https://marketplace-if9n.onrender.com/api/v1/products";
 
  */
 //se aplico axios, la diferencia esque no es necesario transformarlo a formato json, porque ya axios lo hace internamente.
+// line 12 hacemos la llamada al metodo put a la url de la solicitud.
 export const productPut = async (id, product, token) => {
   try {
     const response = await axios.put(`${URL_API}/${id}`, product, {
